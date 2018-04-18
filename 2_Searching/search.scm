d7cf0621ba3b4c392028fd31824ca492ab75e776
Joey Phan <josephkphan@gmail.com>
2018-04-17 23:16:42 -0700

    working is-adjacent function

--- 2_Searching/search.scm
+++ 2_Searching/search.scm
@@ -1,4 +1,72 @@
+;input data 
+(define adjacency-map '(
+    (Alabama Mississippi Tennessee Georgia Florida)
+    (Alaska)
+    (Arkansas Texas Oklahoma Missouri Tennessee Mississippi Louisiana)
+    (Arizona California Nevada Utah New-Mexico)
+    (California Arizona Nevada Oregon)
+    (Colorado New-Mexico Utah Wyoming Nebraska Kansas Oklahoma)
+    (Connecticut New-York Massachusetts Rhode-Island)
+    (Delaware Maryland Pennsylvania New-Jersey)
+    (Florida Alabama Georgia)
+    (Georgia Florida Alabama Tennessee North-Carolina South-Carolina)
+    (Hawaii)
+    (Idaho Oregon Washington Montana Wyoming Utah Nevada)
+    (Indiana Illinois Michigan Ohio Kentucky)
+    (Illinois Missouri Iowa Wisconsin Indiana Kentucky)
+    (Iowa Missouri Illinois Wisconsin Minnesota South-Dakota Nebraska)
+    (Kansas Colorado Nebraska Missouri Oklahoma)
+    (Kentucky Missouri Illinois Indiana Ohio West-Virginia Virginia Tennessee)
+    (Louisiana Texas Arkansas Mississippi)
+    (Maine New-Hampshire)
+    (Maryland Virginia West-Virginia Pennsylvania Delaware)
+    (Massachusetts Rhode-Island Connecticut New-York Vermont New-Hampshire)
+    (Michigan Wisconsin Indiana Ohio)
+    (Minnesota North-Dakota South-Dakota Iowa Wisconsin)
+    (Mississippi Louisiana Arkansas Tennessee Alabama)
+    (Missouri Oklahoma Kansas Nebraska Iowa Illinois Kentucky Tennessee Arkansas)
+    (Montana Idaho Wyoming South-Dakota North-Dakota)
+    (Nebraska Colorado Kansas Missouri Iowa South-Dakota Wyoming)
+    (Nevada California Arizona Utah Idaho Oregon)
+    (New-Hampshire Maine Vermont Massachusetts)
+    (New-Jersey Delaware Pennsylvania New-York)
+    (New-Mexico Texas Oklahoma Colorado Arizona)
+    (New-York Pennsylvania New-Jersey Connecticut Massachusetts Vermont)
+    (North-Carolina South-Carolina Georgia Tennessee Virginia)
+    (North-Dakota Montana South-Dakota Minnesota)
+    (Ohio Michigan Indiana Kentucky West-Virginia Pennsylvania)
+    (Oklahoma Texas New-Mexico Colorado Kansas Missouri Arkansas)
+    (Oregon Washington Idaho Nevada California)
+    (Pennsylvania Ohio West-Virginia Maryland Delaware New-Jersey New-York)
+    (Rhode-Island Connecticut Massachusetts)
+    (South-Carolina Georgia North-Carolina)
+    (South-Dakota Nebraska Iowa Minnesota North-Dakota Montana Wyoming)
+    (Tennessee Arkansas Missouri Kentucky Virginia North-Carolina Georgia Alabama Mississippi)
+    (Texas New-Mexico Oklahoma Arkansas Louisiana)
+    (Utah Nevada Idaho Wyoming Colorado Arizona)
+    (Vermont New-York Massachusetts New-Hampshire)
+    (Virginia North-Carolina Tennessee Kentucky West-Virginia Maryland)
+    (Washington Oregon Idaho)
+    (West-Virginia Virginia Kentucky Ohio Pennsylvania Maryland)
+    (Wisconsin Minnesota Iowa Illinois Michigan)
+    (Wyoming Idaho Montana South-Dakota Nebraska Colorado Utah)
+  )
+)
+
 ;---- Logging Functions
+(define (log-helper message-list)
+    (cond   
+        [ (null? message-list)
+                (newline)
+        ]
+        [ #t 
+            (display (car message-list))
+            (log-helper (cdr message-list))
+        ]
+    )
+)
+; TODO This function doesn't print (1 2 $3) with the value of $3
+
 (define (debug-log message)
     (display "DEBUG: ")
     (display message)
@@ -41,3 +109,55 @@
         (replace-nth-item n2 (replace-nth-item n1 alist (nth-item n2 alist)) temp)
     )
 )
+
+(define (index-of-helper state alist counter)
+    (cond 
+        [
+            (null? alist) 
+                (error-log (cons "could not find state" state) )
+                #f
+        ]
+        [
+            (equal? (car (car alist)) state)
+                counter
+        ]
+        [
+            #t 
+                (index-of-helper state (cdr alist) (+ counter 1))
+        ]
+    )
+)
+
+(define (index-of state alist)
+    (index-of-helper state alist 1)
+)
+;(index-of 'Hawaii adjacency-map)
+
+(define (contains state alist)
+    (cond 
+        [
+            (null? alist) 
+                #f
+        ]
+        [
+            (equal? (car alist) state)
+                #t
+        ]
+        [
+            #t 
+                (contains state (cdr alist))
+        ]
+    )
+)
+;scheme@(guile-user) [15]> (contains 'A '(A B))
+;$16 = #t
+;scheme@(guile-user) [15]> (contains 'A '(B C))
+;$17 = #f
+
+(define (is-adjacent state1 state2)
+    (contains state2 (nth-item (index-of state1 adjacency-map) adjacency-map))
+)
+;scheme@(guile-user) [16]> (is-adjacent 'New-York 'Vermont)
+;$21 = #t
+;scheme@(guile-user) [16]> (is-adjacent 'New-York 'Florida)
+;$22 = #f
